cf754187f240d29d79c20fad748e9cb4097bf0ee
DESKTOP-MSG4FRU\USUARIO <juancamilor576@gmail.com>
2026-03-04 20:39:35 -0500

    feat(index.tsx): create Login component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import "@/styles/globals.css";
 
+
+// const montserrat = Montserrat({
+//   subsets: ["latin"],
+//   variable: "--font-montserrat"
+// })
+
+// const roboto = Roboto_Mono({
+//   subsets:["latin"],
+//   variable: "--font-roboto"
+// })
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <TooltipProvider>
+      <Component {...pageProps} />
+    </TooltipProvider>
+  )
 }
