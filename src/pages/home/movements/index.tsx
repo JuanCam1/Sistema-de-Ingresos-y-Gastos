8d981a74c9f30daad3b77deb12d0a5a23b29599b
DESKTOP-MSG4FRU\USUARIO <juancamilor576@gmail.com>
2026-03-06 20:21:36 -0500

    feat(user): created component list movement

--- src/pages/home/movements/index.tsx
+++ src/pages/home/movements/index.tsx
@@ -1,10 +1,19 @@
+import { columnsMovement } from "@/components/shared/table-movement/columns-movement";
+import { DataTableMovement } from "@/components/shared/table-movement/data-table-movement";
 import HomeLayout from "@/layouts/home-layout";
 import { ReactElement } from "react";
 
 export default function MovementsPage() {
-	return <div>movimientos</div>;
+	return (
+		<div className="flex flex-col justify-center items-center  h-full">
+			<h2 className="text-accent text-5xl font-semibold text-center dark:text-white">
+				Movimientos
+			</h2>
+			<DataTableMovement columns={columnsMovement} />
+		</div>
+	);
 }
 
-MovementsPage.getLayout = function getLayout(page: ReactElement) {
-	return <HomeLayout>{page}</HomeLayout>;
-};
+// MovementsPage.getLayout = function getLayout(page: ReactElement) {
+// 	return <HomeLayout>{page}</HomeLayout>;
+// };
