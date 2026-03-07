ad6b7201783d5b8559f29731e1ac37f5fb06c883
DESKTOP-MSG4FRU\USUARIO <juancamilor576@gmail.com>
2026-03-06 20:18:46 -0500

    feat(api): created endpoint for user filter

---
+++ src/pages/api/user/index.ts
@@ -0,0 +1,14 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { userController } from "@/server/controllers/user-controller";
+
+
+export default async function handler( req: NextApiRequest,res: NextApiResponse ){
+
+  if (req.method === "GET") {
+    return userController.getUsers(req, res);
+  }
+
+  res.status(405).end();
+}
+
+
