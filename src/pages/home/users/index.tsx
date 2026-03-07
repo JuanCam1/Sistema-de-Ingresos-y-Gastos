90e35ca7afb0628f5d20c4852f0a0887a133ada8
DESKTOP-MSG4FRU\USUARIO <juancamilor576@gmail.com>
2026-03-06 20:20:52 -0500

    feat(user): created component list user

--- src/pages/home/users/index.tsx
+++ src/pages/home/users/index.tsx
@@ -1,9 +1,13 @@
+import { columnsUser } from "@/components/shared/table-users/columns-user";
+import { DataTableUser } from "@/components/shared/table-users/data-table-user";
+
 export default function UserPage() {
 	return (
 		<div className="flex flex-col justify-center items-center  h-full">
 			<h2 className="text-accent text-5xl font-semibold text-center dark:text-white">
 				Usuarios
 			</h2>
+			<DataTableUser columns={columnsUser} />
 		</div>
 	);
 }
