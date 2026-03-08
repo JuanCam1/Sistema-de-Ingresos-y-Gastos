1ac4f21aa0187cf6b9172b9aaf47230a5cb74573
DESKTOP-MSG4FRU\USUARIO <juancamilor576@gmail.com>
2026-03-08 18:15:34 -0500

    typing solution

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,10 +1,29 @@
 import { createAuthClient } from "better-auth/react";
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import type { auth } from "./auth";
 
 export const authClient = createAuthClient({
 	baseURL:
 		typeof window !== "undefined"
 			? window.location.origin
 			: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+	plugins: [inferAdditionalFields<typeof auth>()],
 });
 
-export const { signIn, signOut, useSession } = authClient;
+export const { signIn, signOut } = authClient;
+
+type BaseSession = typeof auth.$Infer.Session;
+type AppSession = BaseSession & {
+	user: BaseSession["user"] & {
+		roleId?: number;
+		roleName?: string;
+	};
+};
+
+export const useSession = () => {
+	const session = authClient.useSession();
+	return {
+		...session,
+		data: session.data as AppSession | null,
+	};
+};
